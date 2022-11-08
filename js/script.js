c20bf20b4d18a05918e819f8a9df31334671cd40
Mikelangelo64 <donni2001331@gmail.com>
2022-11-08 14:49:15 +0200

    v-1.7

--- js/script.js
+++ js/script.js
@@ -115,11 +115,27 @@ $(document).ready(function(){
     if(isMobile.any()){
         $('.menu .menu__item__with__dop').click(function(e){
             e.preventDefault()
-            appearDropDown.call(this, e)
+            if($(this).hasClass('_focus')) {
+                appearDropDown.call(this, e)
+            }
+
+            if($(this).is(':focus')) {
+                $(this).addClass('_focus')
+                return
+            }
         })
         $('.menu-big .menu__item__with__dop').click(function(e){
             e.preventDefault()
-            appearDropDown.call(this, e)
+            if($(this).hasClass('_focus')) {
+                appearDropDown.call(this, e)
+            }
+
+            if($(this).is(':focus')) {
+                $(this).addClass('_focus')
+                return
+            }
+            console.log('click');
+            //appearDropDown.call(this, e)
         })
     }else{
         $('.menu .menu__item__with__dop').parent().hover(function(e){
@@ -130,7 +146,11 @@ $(document).ready(function(){
         })
     }
     $('.menu-big .menu__item__with__dop').focus(function(e){
+        console.log('focus');
+        //$(this).off('click')
+        $('.menu-big .menu__item__with__dop').removeClass('_focus')
         appearDropDown.call(this, e)
+        //$(this).on('click')
     })
 
     function appearDropDown(e){
@@ -142,7 +162,6 @@ $(document).ready(function(){
             $('.menu-dropdown').not($(this).next($('.menu-dropdown'))).slideUp(300)
             //return
         //}
-
         // $('.menu .menu__item__with__dop').not($(this)).removeClass('_active-dropdown')
         // $('.menu-dropdown').not($(this).next($('.menu-dropdown'))).slideUp(300)
     }
@@ -164,7 +183,18 @@ $(document).ready(function(){
     //pop-up steps
     $('._open-help').click(function(e){
         e.preventDefault()
-        $('.pop-up').addClass('_pop-up-opened')
+        $('.pop-up._help').addClass('_pop-up-opened')
+
+        $('body').addClass('_lock')
+
+        $('.menu-dropdown').slideUp(300)
+    })
+
+    //popup request
+    $('._popup-open').click(function(e){
+        e.preventDefault()
+        const popup = $(this).attr('data-popup')
+        $(`.${popup}`).addClass('_pop-up-opened')
 
         $('body').addClass('_lock')
 
@@ -172,11 +202,11 @@ $(document).ready(function(){
     })
 
     $('.pop-up__close').click(function(e){
-        $('.pop-up').removeClass('_pop-up-opened')
+        $(this).parent().parent().removeClass('_pop-up-opened')
         $('body').removeClass('_lock')
     })
     $('.pop-up__bg').click(function(e){
-        $('.pop-up').removeClass('_pop-up-opened')
+        $(this).parent().removeClass('_pop-up-opened')
         $('body').removeClass('_lock')
     })
 
@@ -252,6 +282,31 @@ $(document).ready(function(){
         })
     }
 
+    activateToggleProject('.project-toggle')
+
+    function activateToggleProject(section){
+        $(`${section} .project-toggle-content__item`).not($(`${section} .project-toggle-content__item._active-page`)).fadeOut(200)
+
+        $(`${section} .project-toggle-head__item`).click(function(event){
+            //console.log(1, this);
+            //const menuLink = event.target;
+            const goto = $(this).attr('data-toggle-page');
+        // console.log(2, $(goto));
+        if(goto && $(goto)){
+                $(this).addClass("_active-page");
+                $(`${section} ${goto}`).addClass("_active-page");
+
+                $(`${section} .project-toggle-head__item`).not($(this)).removeClass("_active-page");
+                $(`${section} .project-toggle-content__item`).not($(goto)).removeClass("_active-page");
+                $(`${section} .project-toggle-content__item`).not($(goto)).fadeOut(0)
+            
+                $(goto).fadeIn(400);
+            }
+
+            event.preventDefault();
+        })
+    }
+
     //faq-accordion
 
     $('.accordion-item .accordion-item__text .accordion-item__subtitle').slideUp(0)
@@ -286,6 +341,217 @@ $(document).ready(function(){
             const currentList = $(this).children('.report-filter__list')
             $(currentList).fadeToggle(300)
         })
+    }
+
+    //bank-more
+    if(document.body.clientWidth <= 1250) {
+        $('.transfer-list__item').not($('.transfer-list__item._first')).slideUp(0)
+        $('.transfer-item').children('.transfer-item__title__container._additional').slideUp(0)
+        $('.transfer-item').children('.transfer-list._additional .transfer-list__item').slideUp(0)
+    }
+
+    // $(window).resize(function(){
+    //     if(document.body.clientWidth <= 1250) {
+    //         $('.transfer-list__item').not($('.transfer-list__item._first')).slideUp(300)
+    //     } else {
+    //         $('.transfer-list__item').not($('.transfer-list__item._first')).slideDown(300)
+    //         $('.transfer-list__more').parent().removeClass('_opened')
+    //     }
+    // })
+
+    $('.transfer-list__more').click(function() {
+        //$(this).parent().toggleClass('_opened')
+        $(this).parent().parent().children('.transfer-list').toggleClass('_opened')
+        $(this).parent().parent().children('.transfer-item__title__container._additional').slideToggle(300)
+        //$(this).parent().children('.transfer-list__item').not($('.transfer-list__item._first')).slideToggle(300)
+        $(this).parent().parent().children('.transfer-list').children('.transfer-list__item').not($('.transfer-list__item._first')).slideToggle(300)
+    })
+
+    //account more--------------------------
+    if(document.body.clientWidth <= 1100) {
+        $('.account-data__field._additional').slideUp(0)
+    }
+
+    $(window).resize(function(){
+        if(document.body.clientWidth > 1100) {
+        //     $('.account-data__field._additional').slideUp(0)
+        //     $('.account-data').removeClass('_opened')
+        // } else {
+            $('.account-data__field._additional').slideDown(0)
+        }
+    })
+
+    $('.account-data__more').click(function() {
+        $(this).parent().parent().toggleClass('_opened')
+        $(this).parent().parent().children('._additional').slideToggle(300)
+    })
+
+    //bank copy in buffer
+    $('.transfer-item__btn').click(function(e) {
+        e.preventDefault()
+        const select = $(this).attr('data-get')
+        let message = ''
+        let popUp = null
+        let items = []
+
+        if($('.pop-up-buffer')[0]){
+            popUp = $('.pop-up-buffer')
+            $(popUp).removeClass('_active')
+        }
+        if(!select) {
+            message = 'Oooops... something wrong'
+            return null
+        }
+
+        const transfer = Array.from(document.querySelectorAll('.transfer-item')).filter(item => item.dataset.item === select)
+        if(transfer.length === 0) {
+            message = 'Oooops... something wrong'
+            return null
+        }
+
+        const list = Array.from(transfer[0].children).filter(child => (
+            Array.from(child.classList).includes('transfer-list')
+        ))
+        if(list.length === 0) {
+            message = 'Oooops... something wrong'
+            return null
+        }
+
+        list.forEach(item => (
+            items.push(
+                ...Array.from(item.children).filter(child => child.localName === 'a')
+            )
+        ))
+        if(items.length === 0) {
+            message = 'Oooops... something wrong'
+            return null
+        }
+        
+        navigator.clipboard.writeText(items.map(item => item.dataset.value).join(',\n '))
+            .then(() => message = 'Copied!')
+            .catch(() => message = 'Oooops... something wrong')
+            .finally(() => {
+                if(popUp){
+                    $(popUp).addClass('_active')
+                    $(popUp).children().children('.pop-up-buffer__text').html(message)
+                }
+                setTimeout(() => $(popUp).removeClass('_active'), 2000)
+            })
+    })
+
+    $('.transfer-list__item').click(function(e) {
+        e.preventDefault()
+        const data = $(this).attr('data-value')
+        let message = ''
+        let popUp = null
+
+        if($('.pop-up-buffer')[0]){
+            popUp = $('.pop-up-buffer')
+            $(popUp).removeClass('_active')
+        }
+
+        navigator.clipboard.writeText(data)
+            .then(() => {
+                if(data) {
+                    message = 'Copied!'
+                } else {
+                    message = 'Oooops... something wrong'
+                }
+            })
+            .catch(() => {
+                message = 'Oooops... something wrong'
+            })
+            .finally(() => {
+                if(popUp){
+                    $(popUp).addClass('_active')
+                    $(popUp).children().children('.pop-up-buffer__text').html(message)
+                }
+                setTimeout(() => $(popUp).removeClass('_active'), 2000)
+            })
+    })
+
+    //form invalid
+    $('.registration-form input').change(function(){
+        if($(this).is(':invalid')) {
+            $('.registration-form button[type=submit]').addClass('_disabled')
+        }
+
+        if($(this).is(':valid')) {
+            $('.registration-form button[type=submit]').removeClass('_disabled')
+        }
+    })
+
+    //form counter
+    //COUNTER--------
+    function makeMinus(e) {
+        e.preventDefault()
+        let $input = $(this).parent().find('input');
+        let count = parseInt($input.val()) - 1;
+        count = count < 1 ? 0 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    }
+
+    function makePlus(e) {
+        e.preventDefault()
+        console.log(this);
+        let $input = $(this).parent().find('input');
+        console.log($input);
+
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    }
+
+    $('.minus').click(function(e) {
+        makeMinus.call(this, e)
+    });
+    $('.plus').click(function(e) {
+        makePlus.call(this, e)
+    });
+
+    //add more inputs
+    let counter = 1
+    $('.registration-form-more').click(addedInput)
+
+    function addedInput(e){
+        e.preventDefault()
+        const containerClass = $(e.currentTarget).parent()
+        
+        $(
+            `
+            <div class="registration-form__section _pet">
+              <div class="select__container">
+                <select name="pet-${counter}">
+                  <option value="cat">Кішка</option>
+                  <option value="dog">Пес</option>
+                </select>
+              </div>
+
+              <div class="registration-form__counter__container">
+                <p>Кількість тварини</p>
+                <div class="registration-form__counter">
+                    <a href="#" class="minus">-</a>
+                    <input type="number" required min="1" value="0">
+                    <a href="#" class="plus">+</a>
+                </div>
+              </div>
+            </div>
+            `
+        ).insertBefore($(e.currentTarget).parent())
+        
+        $('.minus').off('click')
+        $('.plus').off('click')
+        
+        $('.minus').click(function(e) {
+            makeMinus.call(this, e)
+        });
+        $('.plus').click(function(e) {
+            makePlus.call(this, e)
+        });
+
+        counter++
     }
 
     //swipers
@@ -330,7 +596,7 @@ $(document).ready(function(){
 
             },
         }
-      });
+    });
 
     //swiper-team
 
@@ -371,7 +637,7 @@ $(document).ready(function(){
                 grabCursor: true,
             },
         }
-      });
+    });
 
     //swiper-news
     let nodeNewsSwiper = document.querySelector('.swiper.news-list')
@@ -416,10 +682,10 @@ $(document).ready(function(){
         })
     }
 
-    var swiperSteps = new Swiper(".swiper.pop-up-list", {
+    let swiperRequest = new Swiper(".pop-up._request .swiper.pop-up-list", {
 
         pagination: {
-          el: ".pop-up-list__pagination__container.swiper-pagination",
+          el: ".pop-up._request .pop-up-list__pagination__container.swiper-pagination",
         },
         // navigation: {
         //   nextEl: ".projects__btns__container .swiper-button-next",
@@ -450,7 +716,43 @@ $(document).ready(function(){
 
             },
         }
-      });
+    });
+
+    var swiperSteps = new Swiper(".pop-up._help .swiper.pop-up-list", {
+
+        pagination: {
+          el: ".pop-up._help .pop-up-list__pagination__container.swiper-pagination",
+        },
+        // navigation: {
+        //   nextEl: ".projects__btns__container .swiper-button-next",
+        //   prevEl: ".projects__btns__container .swiper-button-prev",
+        // },
+
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: false,
+        //grabCursor: true,
+
+        breakpoints: {
+            1020:{
+                //freeMode: false,
+                spaceBetween: 42,
+                slidesPerView: 2,
+                centeredSlides: false,
+                grabCursor: true,
+                //slidesPerGroup: 1,
+            },
+            1175:{
+                slidesPerView: 3,
+                centeredSlides: false,
+                grabCursor: true,
+                pagination: false,
+                //slidesPerGroup: 1,
+
+            },
+        }
+    });
 
     $('.pop-up__skip').click(function(e){
         swiperSteps.slideTo(2, 300)
@@ -534,4 +836,116 @@ $(document).ready(function(){
     }
 
 
+    let nodeBankSwiper = document.querySelector('.swiper.bank-list-swiper')
+    let bankSwiper = undefined
+    
+    if(nodeBankSwiper) {
+        $(window).ready(function(){
+            if (document.documentElement.clientWidth < 830) {
+                bankSwiper = new Swiper(nodeBankSwiper, {
+                    navigation: {
+                        nextEl: ".bank-list-swiper__btns__container .swiper-button-next",
+                        prevEl: ".bank-list-swiper__btns__container .swiper-button-prev",
+                    },
+                    pagination: {
+                        el: ".bank-list-swiper__pagination__container",
+                        clickable: true,
+                        renderBullet: function (index, className) {
+                            return '<span class="' + className + '">' + (index + 1) + "</span>";
+                        },
+                    },
+                    slidesPerView: 1,
+                    spaceBetween: 0,
+                    autoHeight: true,
+                    loop: false,
+                })
+            }
+        })
+    
+        $(window).resize(function(){
+            if (document.documentElement.clientWidth < 830) {
+                if(bankSwiper === undefined) {
+                    bankSwiper = new Swiper(nodeBankSwiper, {
+                        navigation: {
+                            nextEl: ".bank-list-swiper__btns__container .swiper-button-next",
+                            prevEl: ".bank-list-swiper__btns__container .swiper-button-prev",
+                        },
+                        pagination: {
+                            el: ".bank-list-swiper__pagination__container",
+                            clickable: true,
+                            renderBullet: function (index, className) {
+                                return '<span class="' + className + '">' + (index + 1) + "</span>";
+                            },
+                        },
+                        slidesPerView: 1,
+                        spaceBetween: 0,
+                        autoHeight: true,
+                        loop: false,
+                    })
+                }
+            } else {
+                if(bankSwiper) {
+                    bankSwiper.destroy()
+                    bankSwiper = undefined
+                }
+            }
+        })
+    }
+
+
+    let nodeRequestSwiper = document.querySelector('.swiper.request-table')
+    let requestSwiper = undefined
+    
+    if(nodeRequestSwiper) {
+        $(window).ready(function(){
+            if (document.documentElement.clientWidth >= 830) {
+                requestSwiper = new Swiper(nodeRequestSwiper, {
+                    navigation: {
+                        nextEl: ".request-table__btns__container .swiper-button-next",
+                        prevEl: ".request-table__btns__container .swiper-button-prev",
+                    },
+                    pagination: {
+                        el: ".request-table__pagination__container",
+                        clickable: true,
+                        renderBullet: function (index, className) {
+                            return '<span class="' + className + '">' + (index + 1) + "</span>";
+                        },
+                    },
+                    slidesPerView: 1,
+                    spaceBetween: 0,
+                    autoHeight: true,
+                    loop: false,
+                })
+            }
+        })
+    
+        $(window).resize(function(){
+            if (document.documentElement.clientWidth >= 830) {
+                if(requestSwiper === undefined) {
+                    requestSwiper = new Swiper(nodeRequestSwiper, {
+                        navigation: {
+                            nextEl: ".request-table__btns__container .swiper-button-next",
+                            prevEl: ".request-table__btns__container .swiper-button-prev",
+                        },
+                        pagination: {
+                            el: ".request-table__pagination__container",
+                            clickable: true,
+                            renderBullet: function (index, className) {
+                                return '<span class="' + className + '">' + (index + 1) + "</span>";
+                            },
+                        },
+                        slidesPerView: 1,
+                        spaceBetween: 0,
+                        autoHeight: true,
+                        loop: false,
+                    })
+                }
+            } else {
+                if(requestSwiper) {
+                    requestSwiper.destroy()
+                    requestSwiper = undefined
+                }
+            }
+        })
+    }
 })
